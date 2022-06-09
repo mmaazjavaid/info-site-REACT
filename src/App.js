d5b478e970923e4a41cc75056861bdc507a66a59
DESKTOP-D5NHP53\Maaz Javaid <mmaazj323@gmail.com>
2022-06-09 19:44:41 +0500

    dark/light mode feature added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react"
+import { useState } from "react"
 import Navbar from "./components/Navbar"
 import Main from "./components/Main"
 import "./style.css";
 export default function App() {
+
+  const [darkMode,setdarkMode]=useState(true);
+  const toggleDarkMode = ()=>{
+    setdarkMode(prevdarkmode=>!prevdarkmode)
+  }
     return (
         <div className="container">
-            <Navbar />
-            <Main />
+            <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+            <Main  darkMode={darkMode} />
         </div>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from "react"
 
 export default function Navbar(props) {
+
+    
     return (
          <nav 
             className={props.darkMode ? "dark": ""}
